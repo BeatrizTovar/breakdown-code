ceaf363ed9cec9484d7e2099f7626f43667f9f39
Beatriz Tovar <beatrizctovar@gmail.com>
2019-04-16 16:07:06 -0700

    Adding bootstrap

---
+++ nonfat-media/src/common/navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Navbar } from "react-bootstrap";
+
+const TopBar = () => {
+  return (
+    <Navbar bg="dark">
+      <Navbar.Brand>NonFat Media Code Challenge</Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default TopBar;
